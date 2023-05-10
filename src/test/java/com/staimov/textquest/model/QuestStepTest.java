15942033fbe9bedd0496cb865ca561717490b1ab
staimov <staimov@mail.ru>
2023-05-10 16:00:26 +0500

    QuestStep unit tests added

---
+++ src/test/java/com/staimov/textquest/model/QuestStepTest.java
@@ -0,0 +1,54 @@
+package com.staimov.textquest.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QuestStepTest {
+    @Test
+    void stepWithEmptyChoicesShouldBeFinal() {
+        QuestStep step = new QuestStep();
+        assertTrue(step.isFinal());
+    }
+
+    @Test
+    void stepWithNotEmptyChoicesShouldNotBeFinal() {
+        QuestStep step = new QuestStep();
+        step.getChoices().add(new QuestChoice());
+        assertFalse(step.isFinal());
+    }
+
+    @Test
+    void finalStepWithGoodTypeShouldBePositiveFinalOnly() {
+        QuestStep step = new QuestStep();
+        step.setType(StepType.GOOD);
+        assertAll(
+            () -> assertTrue(step.isPositiveFinal()),
+            () -> assertFalse(step.isNegativeFinal()),
+            () -> assertFalse(step.isNeutralFinal())
+        );
+
+    }
+
+    @Test
+    void finalStepWithBadTypeShouldBeNegativeFinalOnly() {
+        QuestStep step = new QuestStep();
+        step.setType(StepType.BAD);
+        assertAll(
+            () -> assertTrue(step.isNegativeFinal()),
+            () -> assertFalse(step.isPositiveFinal()),
+            () -> assertFalse(step.isNeutralFinal())
+        );
+    }
+
+    @Test
+    void finalStepWithNeutralTypeShouldBeNeutralFinalOnly() {
+        QuestStep step = new QuestStep();
+        step.setType(StepType.NEUTRAL);
+        assertAll(
+            () -> assertTrue(step.isNeutralFinal()),
+            () -> assertFalse(step.isPositiveFinal()),
+            () -> assertFalse(step.isNegativeFinal())
+        );
+    }
+}
