4a7ff2177e352b878ffe027dcb5c73429a341ac0
staimov <staimov@mail.ru>
2023-05-10 09:40:38 +0500

    choice with radio buttons added

--- src/main/java/com/staimov/textquest/controller/QuestController.java
+++ src/main/java/com/staimov/textquest/controller/QuestController.java
@@ -6,8 +6,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class QuestController {
@@ -34,7 +33,7 @@ public class QuestController {
     }
 
     @GetMapping("/nextStep")
-    public String nextStep(@RequestParam("choiceId") int choiceId, Model model) {
+    public String nextStep(@RequestParam("choiceId") int choiceId) {
         service.nextQuestStep(choiceId);
         return "redirect:/currentStep";
     }
