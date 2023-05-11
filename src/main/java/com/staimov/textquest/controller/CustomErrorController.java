a20aa9ad43852d66223c33349946ce9e96d6195e
staimov <staimov@mail.ru>
2023-05-11 15:51:43 +0500

    custom error handler

---
+++ src/main/java/com/staimov/textquest/controller/CustomErrorController.java
@@ -0,0 +1,46 @@
+package com.staimov.textquest.controller;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+
+    private static final Logger logger = LoggerFactory.getLogger(CustomErrorController.class);
+
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request, Model model) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        Exception e = (Exception) request.getAttribute(RequestDispatcher.ERROR_EXCEPTION);
+
+        String message = "Что-то пошло не так!";
+        logger.error(message);
+
+        if (status != null) {
+            Integer statusCode = Integer.valueOf(status.toString());
+
+            if(statusCode == HttpStatus.NOT_FOUND.value()) {
+                message = "Извините, мы не смогли найти страницу, которую вы ищете.";
+            }
+
+            logger.error("HTTP Status: " + status);
+        }
+
+        if (e != null) {
+            logger.error("Exception: " + e);
+        }
+
+        model.addAttribute("message", message);
+        model.addAttribute("status", status);
+        model.addAttribute("exception", e);
+
+        return "error";
+    }
+}
