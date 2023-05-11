3e0a853ca5865e651de8a0864493020e9e9a0056
staimov <staimov@mail.ru>
2023-05-11 12:32:01 +0500

    springboot context test updated

--- src/test/java/com/staimov/textquest/TextQuestApplicationTests.java
+++ src/test/java/com/staimov/textquest/TextQuestApplicationTests.java
@@ -1,13 +1,20 @@
 package com.staimov.textquest;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class TextQuestApplicationTests {
+	@Autowired
+	private ApplicationContext context;
 
 	@Test
 	void contextLoads() {
+		assertNotNull(context);
 	}
 
 }
