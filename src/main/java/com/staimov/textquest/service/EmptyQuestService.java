11f8b9fabbf862701d1627e6127be6ac048f95ea
staimov <staimov@mail.ru>
2023-05-17 09:27:00 +0500

    updated

--- src/main/java/com/staimov/textquest/service/EmptyQuestService.java
+++ src/main/java/com/staimov/textquest/service/EmptyQuestService.java
@@ -1,6 +1,7 @@
 package com.staimov.textquest.service;
 
 import com.staimov.textquest.model.QuestModel;
+import com.staimov.textquest.model.QuestStep;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +9,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class EmptyQuestService extends AbstractQuestService {
-    private static final Logger logger = LoggerFactory.getLogger(ContactQuestService.class);
+    private static final Logger logger = LoggerFactory.getLogger(DefaultQuestService.class);
 
     @Autowired
     public EmptyQuestService(QuestModel model) {
@@ -18,5 +19,7 @@ public class EmptyQuestService extends AbstractQuestService {
     @Override
     public void initModel() {
         logger.info("Quest init: {}", getClass().getSimpleName());
+        getQuestModel().setRoot(null);
+        getQuestModel().setCurrentStep(null);
     }
 }
