c186a7220f8aa605bef7832852145c2691167b24
staimov <staimov@mail.ru>
2023-05-23 11:42:47 +0500

    Investigate quest added

--- src/main/java/com/staimov/textquest/config/QuestConfig.java
+++ src/main/java/com/staimov/textquest/config/QuestConfig.java
@@ -2,6 +2,7 @@ package com.staimov.textquest.config;
 
 import com.staimov.textquest.model.QuestModel;
 import com.staimov.textquest.service.DefaultQuestService;
+import com.staimov.textquest.service.InvestigateQuestService;
 import com.staimov.textquest.service.QuestService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,7 +13,8 @@ public class QuestConfig {
     @Bean
     @SessionScope
     public QuestService selectedQuestService() {
-        QuestService service = new DefaultQuestService(new QuestModel());
+        QuestService service = new InvestigateQuestService(new QuestModel());
+        //QuestService service = new DefaultQuestService(new QuestModel());
         //QuestService service = new EmptyQuestService(new QuestModel());
         service.initModel();
         return service ;
